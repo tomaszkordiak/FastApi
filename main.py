4c281848e261dc262ab77e1534b9c548d2ee1b89
Tomasz Kordiak <tkordiak@gmail.com>
2021-09-15 18:20:09 +0200

    some endpoints

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Tomek'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the database'}
+    else:
+        return {'data': f'{limit} blogs from db'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return {'data': {'1', '2'}}
+
+
+@app.post('/blog')
+def create_blog():
+    return {'data': 'Blog is created'}
