39a597ebae3b6fcb1e400903928a0d8a3133557c
Tomasz Kordiak <tkordiak@gmail.com>
2021-09-18 14:35:38 +0200

    some endpoints 2

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,13 @@
+from typing import List
+import uvicorn
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from . import schemas, models, database
 from sqlalchemy.orm import Session
 
+from . import database
+from . import models
+from . import schemas
 from .database import SessionLocal
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -26,13 +31,36 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blogs')
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {'status': f'blog {id} deleted'}
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.update(request.dict())
+    db.commit()
+    return {'status': f'blog {id} updated'}
+
+
+@app.get('/blogs', response_model=List[schemas.ShowBlog])
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def get_single_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -42,3 +70,15 @@ def get_single_blog(id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    hashedPassword = Hash.bcrypt(request.password)
+    new_user = models.User(name=request.name, email=request.email, password=hashedPassword)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="localhost", port=8000)
